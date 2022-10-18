6e804812a16efe10d2999e697554f4ad6fb3eed7
Owen-Wang1234 <owen.wang.89@gmail.com>
2022-10-18 17:53:37 -0500

    Corrections made during data import

--- schema.sql
+++ schema.sql
@@ -36,8 +36,8 @@ CREATE TABLE salaries(
 );
 
 CREATE TABLE dept_emp(
-	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
@@ -50,6 +50,5 @@ CREATE TABLE titles(
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
