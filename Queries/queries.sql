9a0fe94415667a18d160396d560ff69eeb0f2245
Owen-Wang1234 <owen.wang.89@gmail.com>
2022-10-19 13:32:58 -0500

    More lists created

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -66,3 +66,56 @@ WHERE de.to_date = ('9999-01-01');
 
 -- Check the table.
 SELECT * FROM current_emp;
+
+-- Employee count by department number.
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO emp_by_dept
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+-- Create table for retiring employee information.
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info
+FROM employees AS e
+INNER JOIN salaries AS s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01');
+
+-- List of managers per department.
+SELECT dm.dept_no,
+	d.dept_name,
+	dm.emp_no,
+	ce.last_name,
+	ce.first_name,
+	dm.from_date,
+	dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+	INNER JOIN departments AS d
+		ON (dm.dept_no = d.dept_no)
+	INNER JOIN current_emp AS ce
+		ON (dm.emp_no = ce.emp_no);
+
+-- List of retirees by deparment.
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO dept_info
+FROM current_emp AS ce
+	INNER JOIN dept_emp AS de
+		ON (ce.emp_no = de.emp_no)
+	INNER JOIN departments as d
+		ON (de.dept_no = d.dept_no);
