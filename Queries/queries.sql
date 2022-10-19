33d446e48f611426ef1ba267ed98c582d0548f00
Owen-Wang1234 <owen.wang.89@gmail.com>
2022-10-19 12:31:30 -0500

    Selected for CURRENT employees eligible for retirement

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -30,3 +30,39 @@ SELECT COUNT(emp_no)
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Drop the initial retirement table.
+DROP TABLE retirement_info;
+
+-- Create new table for retiring employees.
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Check the table.
+SELECT * FROM retirement_info;
+
+-- Joining departments and dept_manager tables.
+SELECT d.dept_name,
+	dm.emp_no,
+	dm.from_date,
+	dm.to_date
+FROM departments AS d
+INNER JOIN dept_manager AS dm
+ON d.dept_no = dm.dept_no;
+
+-- Joining retirement_info and dept_emp tables.
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info AS ri
+LEFT JOIN dept_emp AS de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+-- Check the table.
+SELECT * FROM current_emp;
