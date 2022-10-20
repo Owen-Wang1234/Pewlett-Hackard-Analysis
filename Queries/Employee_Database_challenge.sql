2e9989cc75eef5ec8b34a7894581474e0644c22b
Owen-Wang1234 <owen.wang.89@gmail.com>
2022-10-19 22:27:35 -0500

    Challenge Deliverable 2 completed

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -20,7 +20,7 @@ title
 INTO unique_titles
 FROM retirement_titles
 WHERE (to_date = '9999-01-01')
-ORDER BY emp_no, to_date DESC;
+ORDER BY emp_no ASC, to_date DESC;
 
 -- Count up the employees near retirement by job title.
 SELECT COUNT(emp_no),
@@ -29,3 +29,21 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY COUNT(emp_no) DESC;
+
+-- Compose a table to determine mentorship eligibility.
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibility
+FROM employees AS e
+	INNER JOIN dept_emp AS de
+		ON (e.emp_no = de.emp_no)
+	INNER JOIN titles AS ti
+		ON (e.emp_no = ti.emp_no)
+WHERE (de.to_date = '9999-01-01')
+	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no ASC;
